e64da16d6f04a6541d480fbb74d185cf9b772b1c
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-10-10 19:06:35 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -31,8 +31,8 @@ end
 
 def matrix_lookup(matrix, row, column)
   # Return the matrix's content at row and column
-  puts matrix()()
-   return matrix()()
+  puts matrix(),()
+   return matrix(),()
 end
 
 def matrix_update(matrix, row, column, new_value)
